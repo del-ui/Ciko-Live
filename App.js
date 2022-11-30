e05fce15eb9a1abd2d8f7511bdf3fc490dc2a2c8
del-ui <wambuivictor@students.must.ac.ke>
2022-11-30 13:10:16 +0300

    commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
-import {createStackNavigator}
+import {createStackNavigator} from "@react-navigation/native";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return <Home/>
